7ed330d91a3d2704bf211333998b4ebc7679aee7
James Lennon <jameslennon321@gmail.com>
2016-03-08 15:13:28 -0500

    Added initial dreams controller

---
+++ application/controllers/dream_gen.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dream_Gen extends CI_Controller {
+	
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+}
