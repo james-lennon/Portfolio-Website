b3c4fb19daa618db0089758ea101f9aa836416b1
James Lennon <jameslennon321@gmail.com>
2016-03-08 15:22:04 -0500

    Renamed controller

--- application/controllers/dream_gen.php
+++ application/controllers/dream_gen.php
@@ -2,9 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dream_Gen extends CI_Controller {
-	
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
+		echo "yoo";
 	}
 }
