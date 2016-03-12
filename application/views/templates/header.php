fd438c8588ef732a355e3e2b9a36448cfcda9ff1
James Lennon <jameslennon321@gmail.com>
2016-03-11 22:33:24 -0500

    Added files

---
+++ application/views/templates/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js">
+<head>
+    <meta charset="utf-8">
+    <title><? if(isset($title)) { echo $title; }  ?></title>
+    <meta name="description" content="">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <meta name="mobile-web-app-capable" content="yes">
+
+    <link rel="stylesheet" href="<?= base_url() ?>res/semantic/dist/semantic.min.css">
+    <script src="<?= base_url() ?>res/semantic/dist/semantic.min.js"></script>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,300&subset=latin,vietnamese' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
