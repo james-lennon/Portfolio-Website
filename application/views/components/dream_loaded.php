b77b1ec16a6be20a93ef6d7e43df85828152601e
James Lennon <jameslennon321@gmail.com>
2016-03-15 21:01:09 -0700

    Added new files

---
+++ application/views/components/dream_loaded.php
@@ -0,0 +1,14 @@
+<h2 class="ui green dividing header">
+	<i class="check circle icon"></i>
+	Dream Generated Successfully!
+</h4>
+<div class="ui center aligned grid">
+	<div class="bottom aligned row">
+		<div class="column">
+			<a class="ui huge button" id="view_dream_btn" href="<?= $dream_url ?>" target="_blank">
+				<i class="unhide icon"></i>
+				View PDF
+			</a>
+		</div>
+	</div>
+</div>
