1db5ee69bccbe04a0115c958f69794c960aad01f
James Lennon <jameslennon321@gmail.com>
2016-03-17 13:30:30 -0700

    Working on submodule

--- application/models/dreams.php
+++ application/models/dreams.php
@@ -6,9 +6,11 @@ class Dreams extends CI_Model {
 	public function generate_dream ($theme = false) {
 		$this->load->helper("url");
 		$result = [];
-		exec("./application/bin/dream_gen/main.py -n 1 -q -i", $result);
-		// exec("python", $result);
+		$return_var = -1;
+		exec("./application/bin/dream_gen/main.py -n 1 -q -i", $result, $return_var);
+		// exec("python run.py", $result, $return_var);
 		var_dump($result);
+		var_dump($return_var);
 		// TODO: check for bad JSON
 		return json_decode(implode($result,''));
 	}
