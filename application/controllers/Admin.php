22fd593b4ddc67fae4d9c6faa10636ba954fa1e5
James Lennon <jameslennon321@gmail.com>
2016-03-17 20:48:19 -0700

    Added a lot of login logic

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,6 +3,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
-	
+	public function index () {
+		check_login();
+	}
+
+	public function login() {
+		$this->load->helper("url");
+
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('email', 'Email', 'required');
+		$this->form_validation->set_rules('password', 'Password', 'required');
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view("pages/admin_login");
+		}
+		else
+		{
+			$email    = $_POST['email'];
+			$password = $_POST['password'];
+
+			$this->load->model('admin_model');
+			if ($this->admin_model->check_login($email, $password)) {
+				redirect("admin/index");
+			}
+		}
+	}
 
 }
--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -3,4 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin_Model extends CI_Model {
 
+	public function check_login ($email, $password) {
+		return true;
+		$this->load->database();
+
+		$this->db->query(
+				"SELECT user.id, user.password
+				 FROM user WHERE user.email = ?",
+				 [$email, password_hash($password)]);
+
+		if ($this->db->num_rows() == 0)
+			return false;
+
+		$result = password_verify($password, $this->db->row()->password);
+
+		if ($result) {
+			$this->load->library('session');
+			$this->session->set_userdata('user_id', $this->db->row()->user_id);
+		}
+
+		return $result;
+	}
+
+	public function is_logged_in () {
+		$this->load->library('session');
+		return $this->session->userdata('user_id');
+	}
+
 }
