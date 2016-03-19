9f98e7e85c87779de78ed25d588ecaf3eebbed44
James Lennon <jameslennon321@gmail.com>
2016-03-19 16:21:27 -0700

    More changes

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -4,30 +4,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin_Model extends CI_Model {
 
 	public function check_login ($email, $password) {
-		return true;
 		$this->load->database();
 
-		$this->db->query(
-				"SELECT user.id, user.password
-				 FROM user WHERE user.email = ?",
-				 [$email, password_hash($password)]);
+		$query = $this->db->query(
+				"SELECT admin.id, admin.password
+				 FROM admin WHERE admin.email = ?",
+				 [$email]);
 
-		if ($this->db->num_rows() == 0)
+		if ($query->num_rows() == 0)
 			return false;
 
-		$result = password_verify($password, $this->db->row()->password);
+		$result = password_verify($password, $query->row()->password);
 
 		if ($result) {
 			$this->load->library('session');
-			$this->session->set_userdata('user_id', $this->db->row()->user_id);
+			$this->session->set_userdata('user_id', $query->row()->user_id);
 		}
-
 		return $result;
 	}
 
 	public function is_logged_in () {
 		$this->load->library('session');
 		return $this->session->userdata('user_id');
+	}
+
+	public function add_admin ($email, $password, $permissions = 0) {
+
 	}
 
 }
