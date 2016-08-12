4c2597b45e76457d61496b1ebe9743d5d0720573
James Lennon <jameslennon321@gmail.com>
2016-08-12 12:15:28 -0400

    Improved dream generator look

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,32 @@
 
+<div class="ui inverted vertical footer segment" id="homepage-footer">
+	<div class="ui container">
+	  <div class="ui stackable inverted divided equal height stackable grid">
+		<div class="three wide column">
+		  <h4 class="ui inverted header">Contact</h4>
+		  <div class="ui inverted link list">
+			<a href="#" class="item">Email</a>
+			<a href="#" class="item">LinkedIn</a>
+			<a href="#" class="item">Facebook</a>
+		  </div>
+		</div>
+		<div class="three wide column">
+		  <h4 class="ui inverted header">Pages</h4>
+		  <div class="ui inverted link list">
+			<a href="/" class="item">Project Portfolio</a>
+			<a href="/dream_gen" class="item">Dream Generator</a>
+		  </div>
+		</div>
+		<div class="seven wide column">
+		  <h4 class="ui inverted header">Footer Header</h4>
+		  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+		</div>
+	  </div>
+	  <div class="ui center aligned container">
+		</p>&copy; 2016 James Lennon</p>
+	  </div>
+	</div>
+</div>
+
 </body>
 </html>
